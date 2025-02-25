2ed19a7b45e0fbbfc639c434718cd87d4f3f087f
marym <Marymfoda138@gmail.com>
2025-02-26 00:02:29 +0200

    add rating repo

--- src/main/java/com/example/Alfayomi/repo/RateRepo.java
+++ src/main/java/com/example/Alfayomi/repo/RateRepo.java
@@ -1,2 +1,10 @@
-package com.example.Alfayomi.repo;public interface RateRepo {
+package com.example.Alfayomi.repo;
+
+import com.example.Alfayomi.entity.Rate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RateRepo extends JpaRepository<Rate , Long> {
+    List<Rate> findByProductId(Long productId);
 }
