20d891090d0d720595863126c340fb7d9d89b1f7
marym <Marymfoda138@gmail.com>
2025-02-25 18:17:28 +0200

    update

--- src/main/java/com/example/Alfayomi/AlfayomiApplication.java
+++ src/main/java/com/example/Alfayomi/AlfayomiApplication.java
@@ -6,10 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.example.Alfayomi")
-@EnableJpaRepositories(basePackages = "com.example.Alfayomi.repository")
 public class AlfayomiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AlfayomiApplication.class, args);
 	}
