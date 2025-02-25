a72ec137d709bb40493511e3228a51acecfed1e1
marym <Marymfoda138@gmail.com>
2025-02-25 18:20:59 +0200

    implememt mapper

--- src/main/java/com/example/Alfayomi/mapper/ProductMapper.java
+++ src/main/java/com/example/Alfayomi/mapper/ProductMapper.java
@@ -1,2 +1,19 @@
-package com.example.Alfayomi.mapper;public class ProductMapper {
+package com.example.Alfayomi.mapper;
+
+import com.example.Alfayomi.dto.ProductDTO;
+import com.example.Alfayomi.entity.Product;
+
+import static com.example.Alfayomi.dto.ProductDTO.*;
+
+public class ProductMapper {
+
+    public static ProductDTO fromEntityToDto(Product product){
+        return builder()
+                .name(product.getName())
+                .price(product.getPrice())
+                .description(product.getDescription())
+                .stock(product.getStock())
+//                .productImages(product.getProductImages())
+                .build();
+    }
 }
